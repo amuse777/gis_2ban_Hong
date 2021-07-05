2c5e19f9c3fcc5499d73624db3054af88e1959e0
amuse <amuse1337@naver.com>
2021-07-05 16:34:41 +0900

    7/1 2ban Project Hong

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accountapp.views import Hello_world
 
+
 urlpatterns = [
     path('hello_world/', Hello_world, name='hello_world'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def Hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
