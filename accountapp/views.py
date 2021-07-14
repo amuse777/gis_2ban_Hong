c00a536f60af6165bb4b2d4d5b60dbd6d811475e
amuse <amuse1337@naver.com>
2021-07-14 11:26:21 +0900

    7/14 Create - 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -32,3 +32,7 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/Create.html'
+
+    #django에서 기본으로 제공하는 것이 Class, Base, View다.
+
+    #Create - Read - Update - Delete
