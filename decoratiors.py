54c095de3d023a7b3203060c9f2483653e473938
RapaHello <amuse1337@naver.com>
2021-07-21 12:40:44 +0900

    7/21 decorator - 2

--- decoratiors.py
+++ decoratiors.py
@@ -1,14 +1,34 @@
+# def decorator(func):
+#     def decorated(input_text):
+#         print('함수 시작')
+#         func(input_text)
+#         print('함수 끝!')
+#     return decorated
+#
+# @decorator
+# def hello_world(input_text):
+#     print(input_text)
+#
+#
+# hello_world('Hello_world')
+
 
 def decorator(func):
-    def decorated(input_text):
-        print('함수 시작')
-        func(input_text)
-        print('함수 끝!')
-    return decorated
-
-@decorator
-def hello_world(input_text):
-    print(input_text)
+    def decorated(hei, based):
+        if hei > 0 and based > 0:
+            return func(hei, based)
+        else:
+            return ValueError('No!')
+    return decorated()
 
 
-hello_world('Hello_world')
+@ decorator
+def tri_area(hei, based):
+    print(hei * based / 2)
+
+@ decorator
+def squ_area(hei, based):
+    print(hei * based)
+
+tri_area(10, 10)
+squ_area(3, 4)
